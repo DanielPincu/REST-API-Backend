acfb34859e56d0b7c841ae629d7e5974c7067b61
DanielPincu <ghepin01@easv365.dk>
2026-01-24 12:14:07 +0100

    CORS, added

--- src/app.ts
+++ src/app.ts
@@ -2,13 +2,26 @@ import express, { Application, Request, Response } from 'express';
 import dotenfFlow from 'dotenv-flow';
 import routes from './routes';
 import { connect } from './repository/database';
+import cors from 'cors';
 
 // Load environment variables from .env files
 dotenfFlow.config();
 
 const app: Application = express();
 
+function setupCors() {
+    app.use(cors({
+        origin: '*',
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
+        allowedHeaders: ['auth-token', 'Origin', 'X-Requested-With', 'Content-Type', 'Accept'],
+        credentials: true,
+    }));
+}
+
+
 export function startServer() {
+
+    setupCors();
 
     app.use(express.json());
     
